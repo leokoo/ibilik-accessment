6c6265c8f23e2477b27712eb361b2441e9dc2d5b
Leo Koo <leokoo@gmail.com>
2015-12-08 17:12:36 +0800

    Tidied up the user interface

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
-
+	validates :length,
+		presence: true
 # Associations
 	belongs_to :user
 	belongs_to :property
