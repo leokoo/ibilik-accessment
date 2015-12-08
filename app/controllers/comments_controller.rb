76c2019277afd079e45cc40581ed682ed68cec61
Leo Koo <leokoo@gmail.com>
2015-12-08 12:52:54 +0800

    CRUD done for comments!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,37 +1,31 @@
-# Create new answer
+# Create new comment
 post '/comments' do
 	comment = Comment.create(reply: params[:reply], user_id: session[:user_id], property_id: params[:property_id])
 		redirect "/properties/#{comment.property.id}"
-		# erb :"answers/new"
 end
 
-# View All Answers
-get '/answers/all' do
-	erb :"answers/all"
+# View comment
+get '/comments/:id' do
+	@comment = Comment.find(params[:id])
+	erb :'comments/show'
 end
 
-# View answer
-get '/answers/:id' do
-	@answer = Answer.find(params[:id])
-	erb :'answers/show'
-end
-
-# Display answer edit form
-get '/answers/:id/edit' do
-	@answer = Answer.find(params[:id])
-	erb :'answers/edit'
+# Display comment edit form
+get '/comments/:id/edit' do
+	@comment = Comment.find(params[:id])
+	erb :'comments/edit'
 end 
 
-# # Update question
-# patch '/questions/:id' do
-# 	question = Question.find(params[:id])
-# 	question.update(title: params[:title], user_id: session[:user_id])
-# 	redirect "/questions/#{question.id}"
-# end
+# Update comment
+patch '/comments/:id' do
+	comment = Comment.find(params[:id])
+	comment.update(reply: params[:reply])
+	redirect "/comments/#{comment.id}"
+end
 
-# # Delete question
-# delete '/questions/:id' do
-# 	question = Question.find(params[:id])
-# 	question.destroy
-# 	erb :'static/index'
-# end
+# Delete comment
+delete '/comments/:id' do
+	comment = Comment.find(params[:id])
+	comment.destroy
+	erb :'static/index'
+end
