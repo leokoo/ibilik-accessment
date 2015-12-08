7402319fb79dcf017a365343f1becd554eceafac
Leo Koo <leokoo@gmail.com>
2015-12-08 19:03:31 +0800

    renamed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # Creating the new user
 post '/signup' do 
 	user = User.new(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+	# user = User.new(params[:user])
+	#params[:user] => 
 	if user.save
 		session[:user_id] = user.id
 		redirect "users/#{user.id}"
